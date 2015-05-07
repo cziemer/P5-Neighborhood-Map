325f0f87ee57d6f9c2ede7caf05fd969f83c1328
Chris Z <christopher.ziemer@gmail.com>
2015-05-07 00:36:38 -0400

    InfoWindow populates basic data from Foursquare

--- js/app.js
+++ js/app.js
@@ -1,26 +1,58 @@
 var model={
 	attractions: [
-		{ name: 'Magic Kingdom Park', lat: 28.417663, long: -81.581212, zoom: 11},
-		{ name: 'EPCOT', lat: 28.374694, long: -81.549404, zoom: 11},
-		{ name: 'Disney Hollywood Studios', lat: 28.357529, long: -81.558271, zoom: 11},
-		{ name: 'Disney Animal Kingdom', lat: 28.359719, long: -81.591313, zoom: 11},
-		{ name: 'Downtown Disney', lat: 28.370174, long: -81.521017, zoom: 11},
-		{ name: 'Universal Orlando Resort', lat: 28.474321, long: -81.467819, zoom: 11},
-		{ name: 'Universal Islands of Adventure', lat: 28.47114, long: -81.471565, zoom: 11},
-		{ name: 'Universal CityWalk', lat: 28.4731495, long: -81.465465, zoom: 11},
-		{ name: 'Seaworld Orlando', lat: 28.411456, long: -81.461705, zoom: 11},
-		{ name: 'Fun Spot America', lat: 28.465445, long: -81.455462, zoom: 11},
-		{ name: 'Wet n Wild Orlando', lat: 28.461565, long: -81.465523, zoom: 11},
-		{ name: 'Disney Typhoon Lagoon', lat: 28.365838, long: -81.529606, zoom: 11},
-		{ name: 'Disney Blizzard Beach', lat: 28.351849, long: -81.575296, zoom: 11}
+		{ name: 'Magic Kingdom Park', lat: 28.417663, long: -81.581212, zoom: 11, fourID: '4b11d311f964a520758523e3'},
+		{ name: 'EPCOT', lat: 28.374694, long: -81.549404, zoom: 11, fourID: '4b301d74f964a52053f624e3'},
+		{ name: 'Disney Hollywood Studios', lat: 28.357529, long: -81.558271, zoom: 11, fourID: '4acb60d2f964a52094c320e3'},
+		{ name: 'Disney Animal Kingdom', lat: 28.359719, long: -81.591313, zoom: 11, fourID: '4b058693f964a5206e6622e3'},
+		{ name: 'Downtown Disney', lat: 28.370174, long: -81.521017, zoom: 11, fourID: '4ac9055ef964a52012be20e3'},
+		{ name: 'Universal Studios Florida', lat: 28.474321, long: -81.467819, zoom: 11, fourID: '4ad62c90f964a5208c0521e3'},
+		{ name: 'Universal Islands of Adventure', lat: 28.47114, long: -81.471565, zoom: 11, fourID: '4b59beb8f964a520c99528e3'},
+		{ name: 'Universal CityWalk', lat: 28.4731495, long: -81.465465, zoom: 11, fourID: '4b058693f964a520726622e3'},
+		{ name: 'Seaworld Orlando', lat: 28.411456, long: -81.461705, zoom: 11, fourID: '4b058692f964a520536622e3'},
+		{ name: 'Fun Spot America', lat: 28.465445, long: -81.455462, zoom: 11, fourID: '4b058691f964a520446622e3'},
+		{ name: 'Wet n Wild Orlando', lat: 28.461565, long: -81.465523, zoom: 11, fourID: '4b97ade0f964a520360f35e3'},
+		{ name: 'Disney Typhoon Lagoon', lat: 28.365838, long: -81.529606, zoom: 11, fourID: '4b0586a2f964a520a66822e3'},
+		{ name: 'Disney Blizzard Beach', lat: 28.351849, long: -81.575296, zoom: 11, fourID: '4b0586a2f964a520a56822e3'},
+		{ name: 'Gatorland', lat: 28.355462, long: -81.403874, zoom: 11, fourID: '4b058691f964a5203a6622e3'},
+		{ name: 'Aquatica', lat: 28.415581, long: -81.456631, zoom: 11, fourID: '4b0586a2f964a520a86822e3'},
+		{ name: 'ESPN Wide World of Sports', lat: 28.337806, long: -81.551746, zoom: 11, fourID: '4b802c2bf964a520185930e3'},
+		{ name: 'Wonderworks', lat: 28.433721, long: -81.471739, zoom: 11, fourID: '4b058693f964a520916622e3'},
+		{ name: 'Holy Land Experience', lat: 28.495742, long: -81.432944, zoom: 11, fourID: '4b6de9eff964a520629b2ce3'}
+		
 	]
 };
 
-function createContent(name) {
-	var contentString;
-	contentString = "<h2>" + name + "</h2><hr>Some info here";
+function buildContent(map, marker, fourID) {
+	var name;
+	var address;
+	var phone;
+	var desc;
+	var stat;
 	
-	return contentString;
+	var fsURL = "https://api.foursquare.com/v2/venues/" + fourID + "?client_id=1GDZWF2EXB4AHKVRWXSFXA5REWFR2KHC2TY3PTDW1IVSHORE&client_secret=EMPTPKLVHRRX2DYVHU4M3KUIDJ5YBJJVUQ05GSOUXF555KQN&v=20150506";
+	$.getJSON(fsURL, function(data) {
+		name = data.response.venue.name;
+		address = data.response.venue.location.formattedAddress;
+		phone = data.response.venue.contact.formattedPhone;
+		desc = data.response.venue.description;
+		url = data.response.venue.url;
+		
+		infoContent = "<h2>" + name + "</h2>";
+		if (address != undefined) {
+			infoContent += address + "<br>";
+		}
+		if (phone != undefined) {
+			infoContent += phone + "<hr>";
+		}
+		if (url != undefined) {
+			infoContent += "URL: <a href='" + url + "'>" + url + "</a><br>";
+		}
+		if(desc != undefined) {
+			infoContent += "<p>" + desc + "</p>";
+		}
+		
+		addMarkerListener(map, marker, fourID, infoContent)
+	});
 }
 
 function clearMarkers(markers){
@@ -31,8 +63,19 @@ function clearMarkers(markers){
     return markers;
 }
 
-function setMarkers(map, name, lat, long, z){
+function addMarkerListener(map, marker, fourID, infoContent) {
 	var infoWindow;
+	infowindow = new google.maps.InfoWindow();
+	google.maps.event.addListener(marker, 'click', function() {
+		//buildContent(fourID);
+		infowindow.setContent(infoContent);
+		infowindow.open(map, marker);
+		map.panTo(marker.position);
+	});
+	
+}
+
+function setMarkers(map, name, lat, long, z, fourID){
     var myLatLng = new google.maps.LatLng(lat, long);
     var marker = new google.maps.Marker({
         position: myLatLng,
@@ -40,14 +83,7 @@ function setMarkers(map, name, lat, long, z){
         title: name,
         zIndex: z
     });
-    
-    infowindow = new google.maps.InfoWindow();
-    
-	google.maps.event.addListener(marker, 'click', function() {
-		infowindow.setContent(createContent(name));
-		infowindow.open(map, marker);
-		map.panTo(marker.position);
-	});
+    buildContent(map, marker, fourID);
 	return marker;
 }
     
@@ -57,6 +93,9 @@ var viewModel = function () {
     self.filteredArray = ko.observableArray();
     self.filter = ko.observable('');
     self.markers = [];
+    self.infoWindowContent = ko.pureComputed(function() {
+	    return $("#infoWindow").html();
+    });
     
     //Set variables for the map
     var mapOptions = {
@@ -77,7 +116,7 @@ var viewModel = function () {
 		self.markers = clearMarkers(self.markers);
 		for (var i = 0; i < tmpArray.length; i++) {
 			loc = tmpArray[i];
-			self.markers.push(setMarkers(map, loc.name, loc.lat, loc.long, loc.z));
+			self.markers.push(setMarkers(map, loc.name, loc.lat, loc.long, loc.z, loc.fourID));
 		}
 		return tmpArray;
     });
@@ -90,9 +129,6 @@ var viewModel = function () {
 		for (var i = 0; i < self.filteredArray().length; i++) {
 			loc = self.filteredArray()[i].name.toLowerCase();
 			if (clickedPlace === loc) {
-				console.log(clickedPlace);
-				console.log(loc);
-				console.log(self.markers.length);
 				google.maps.event.trigger(self.markers[i], 'click');
 			}
 		}
